504359e49d99bc7aff024947dd5780e1a27b1587
Lateefut <adebanjolateef18@gmail.com>
2022-04-08 17:55:26 -0700

    prog mul

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,11 @@
-#include <stdio.h>                                                                                                                                                      
-
-/**                                                                                                                                                                     
-
-* main - Entry Point                                                                                                                                                    
-
-*                                                                                                                                                                       
-
-* Return: 0 means success                                                                                                                                               
-
-*/ 
-
-int main (void)                                                                                                                                                                    
-
-{                                                                                                                                                                       
-
-puts("\"Programming is like a multilingual puzzle");                                                                                                                    
-
-return(0);
-
+#include <stdio.h>
+/**
+*main - entry point
+*
+*Return: 0 after printing the function
+*/
+int main(void)
+{
+puts("\"Programmming is like building a multilingual puzzle");
+return (0);
 }
