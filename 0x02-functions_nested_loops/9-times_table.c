4f698acf6fc899bbbb10ede68b848b455c12349b
Lateefut <adebanjolateef18@gmail.com>
2022-04-13 13:05:56 -0700

    Times Table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,33 +9,32 @@ void times_table(void)
 	int row;
 	int column;
 	int product;
-	
-	for (row = 0; row <=9; row++)
+
+	for (row = 0; row <= 9; row++)
 	{
+		for (column = 0; column <= 9; column++)
+		{
+			product = (row * column);
 
-        	for (column = 0; column <= 9; column++)
-	        {
-	        	product = (row * column);
-
-          		if (column == 0)
-	        	{
-		        	_putchar('0' + product);
-		        }
-          		else if (product <= 9)
-	        	{
-		        	_putchar(',');
-                 		_putchar(' ');
-               			_putchar(' ');
-	         		_putchar('0' + product);
-	        	}
-                 	else if (product > 9)
-            		{
-	          		_putchar(',');
-          			_putchar(' ');
-         			_putchar('0' + (product / 10));
-          			_putchar('0' + (product % 10));
-	        	}
-        	}
+			if (column == 0)
+			{
+				_putchar('0' + product);
+			}
+			else if (product <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + product);
+			}
+			else if (product > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
+			}
+		}
 		_putchar('\n');
 	}
 }
