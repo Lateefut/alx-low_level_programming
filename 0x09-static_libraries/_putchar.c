21c0048eb01086564541b66a53c752791b7db8b6
Lateefut <adebanjolateef18@gmail.com>
2022-05-02 16:47:57 -0700

    putchar

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -3,7 +3,7 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: the character to print
- * Return: on success 1,
+ * Return: on success 1.
  * on error, -1 is returned, and error is set appropriately.
  */
 int _putchar(char c)
