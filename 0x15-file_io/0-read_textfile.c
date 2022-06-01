e99b4911889923af613500ef18d288b96ff9c2f8
Lateefut <adebanjolateef18@gmail.com>
2022-06-01 07:05:00 -0700

    functin that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,7 +4,7 @@
  * read_textfile - read a certain size and prints to std output
  * @filename: file to read from
  * @letters: size to read
- * Read: actual size read and printed
+ * Return: actual size read and printed
  */
 
 ssize_t read_textfile(const char *filename, ssize_t letters)
